207613cb3962f91baee1d83263a6b26aef0f4290
CesarCruz001 <60804673+CesarCruz001@users.noreply.github.com>
2021-01-27 23:22:35 -0600

    Add files via upload
    Corrigiendo errores

--- c211-aztecas-master/lib/lexer.ex
+++ c211-aztecas-master/lib/lexer.ex
@@ -1,58 +1,101 @@
 defmodule Lexer do
   def scan_words(words) do
-    Enum.flat_map(words, &lex_raw_tokens/1)
+      text=Enum.map(words, fn {string,_} -> string end)
+      line=Enum.map(words, fn {_,number} -> number end)
+      text
+      |> Enum.zip(line)
+      |> Enum.flat_map(&lex_raw_tokens(elem(&1, 0), elem(&1, 1)))
+      |> check_error()
   end
 
-  def get_constant(program) do
+  def check_error(listLex) do
+    if Enum.find_value(listLex, false, fn(x)->x==:error end)==true do
+      {:error}
+
+  else
+    listLex
+  end
+  end
+
+  def get_constant(program,line) do
+    invalid=program
     case Regex.run(~r/^\d+/, program) do
       [value] ->
-        {{:constant, String.to_integer(value)}, String.trim_leading(program, value)}
+        {{:constant, String.to_integer(value)},line, String.trim_leading(program,value)}
 
       program ->
-        {:error, "Token not valid: #{program}"}
+       IO.inspect({:error, invalid, "token not valid at line: " <> to_string(line)})
+       {:error, line, "Token not valid: #{program}"}
     end
   end
 
-  def lex_raw_tokens(program) when program != "" do
-    {token, rest} =
-      case program do
+  def lex_raw_tokens(program,line) when program != "" and not is_tuple(program) do
+      trimmed_content = String.trim(program)
+      sal=String.replace(trimmed_content, " ","")
+
+
+    {token,lin,rest} =
+      case sal do
         "{" <> rest ->
-          {:open_brace, rest}
+          {:open_brace,line, rest}
 
         "}" <> rest ->
-          {:close_brace, rest}
+          {:close_brace,line, rest}
 
         "(" <> rest ->
-          {:open_paren, rest}
+          {:open_paren,line, rest}
 
         ")" <> rest ->
-          {:close_paren, rest}
+          {:close_paren,line, rest}
 
         ";" <> rest ->
-          {:semicolon, rest}
+          {:semicolon,line, rest}
 
         "return" <> rest ->
-          {:return_keyword, rest}
+          {:return_keyword,line, rest}
 
         "int" <> rest ->
-          {:int_keyword, rest}
+          {:int_keyword,line, rest}
 
         "main" <> rest ->
-          {:main_keyword, rest}
+          {:main_keyword,line, rest}
 
-        rest ->
-          get_constant(rest)
+        #Operadores unarios para segunda entrega.
+
+        "-" <> rest ->
+          {:minus, line, rest}
+
+        "~" <> rest ->
+          {:bitwise, line, rest}
+
+        "!" <> rest ->
+          {:logical_negation, line, rest}
+
+         #Operadores binarios para tercera entrega.
+
+         "+" <> rest ->
+          {:addition, line, rest}
+
+         "*" <> rest ->
+          {:multiplication, line, rest}
+
+         "/" <> rest ->
+          {:division, line, rest}
+
+          rest ->
+            get_constant(rest,line)
+
       end
 
     if token != :error do
-      remaining_tokens = lex_raw_tokens(rest)
-      [token | remaining_tokens]
+      remaining_tokens = lex_raw_tokens(rest,line)
+      [{token,lin} | remaining_tokens]
     else
       [:error]
     end
   end
 
-  def lex_raw_tokens(_program) do
+  def lex_raw_tokens(_program,_line) do
     []
   end
 end
--- c211-aztecas-master/lib/parser.ex
+++ c211-aztecas-master/lib/parser.ex
@@ -2,6 +2,7 @@ defmodule Parser do
   def parse_program(token_list) do
     function = parse_function(token_list)
 
+
     case function do
       {{:error, error_message}, _rest} ->
         {:error, error_message}
@@ -15,18 +16,34 @@ defmodule Parser do
     end
   end
 
-  def parse_function([next_token | rest]) do
+  def parse_function([tup_token | rest]) do
+    next_token=elem(tup_token, 0)
+    lineInt=elem(tup_token, 1)
+
     if next_token == :int_keyword do
-      [next_token | rest] = rest
+
+      [tup_token | rest] = rest
+      next_token=elem(tup_token,0)
+      lineMain=elem(tup_token, 1)
+
 
       if next_token == :main_keyword do
-        [next_token | rest] = rest
+        [tup_token | rest] = rest
+        next_token=elem(tup_token,0)
+        lineOparen=elem(tup_token, 1)
+
 
         if next_token == :open_paren do
-          [next_token | rest] = rest
+          [tup_token | rest] = rest
+          next_token=elem(tup_token,0)
+          lineCparen=elem(tup_token, 1)
+
 
           if next_token == :close_paren do
-            [next_token | rest] = rest
+            [tup_token | rest] = rest
+            next_token=elem(tup_token,0)
+            lineObrace=elem(tup_token, 1)
+
 
             if next_token == :open_brace do
               statement = parse_statement(rest)
@@ -35,56 +52,269 @@ defmodule Parser do
                 {{:error, error_message}, rest} ->
                   {{:error, error_message}, rest}
 
-                {statement_node, [next_token | rest]} ->
+                {statement_node, [tup_token | rest]} ->
+                  next_token=elem(tup_token, 0)
+                  lineCbrace=elem(tup_token, 1)
+
                   if next_token == :close_brace do
                     {%AST{node_name: :function, value: :main, left_node: statement_node}, rest}
                   else
-                    {{:error, "Error, close brace missed"}, rest}
+                    {{:error, "Error, close brace missed in line " <> Integer.to_string(lineCbrace)}, rest}
                   end
               end
             else
-              {:error, "Error: open brace missed"}
+              {:error, "Error: open brace missed in line " <> Integer.to_string(lineObrace)}
             end
           else
-            {:error, "Error: close parentesis missed"}
+            {:error, "Error: close parentesis missed in line " <> Integer.to_string(lineCparen)}
           end
         else
-          {:error, "Error: open parentesis missed"}
+          {:error, "Error: open parentesis missed in line " <> Integer.to_string(lineOparen)}
         end
       else
-        {:error, "Error: main functionb missed"}
+        {:error, "Error: main function missed in line " <> Integer.to_string(lineMain)}
       end
     else
-      {:error, "Error, return type value missed"}
+      {:error, "Error, return type value missed in line " <> Integer.to_string(lineInt)}
     end
   end
 
-  def parse_statement([next_token | rest]) do
+  def parse_statement([tup_token | rest]) do
+    next_token=elem(tup_token, 0)
+    lineRet=elem(tup_token, 1)
+
     if next_token == :return_keyword do
+
       expression = parse_expression(rest)
 
       case expression do
         {{:error, error_message}, rest} ->
           {{:error, error_message}, rest}
 
-        {exp_node, [next_token | rest]} ->
+        {exp_node, [tup_token | rest]} ->
+          next_token=elem(tup_token,0)
+          lineSemicolon=elem(tup_token, 1)
+
           if next_token == :semicolon do
             {%AST{node_name: :return, left_node: exp_node}, rest}
           else
-            {{:error, "Error: semicolon missed after constant to finish return statement"}, rest}
+
+            {{:error, "Error: semicolon missed after constant to finish return statement in line " <> Integer.to_string(lineSemicolon)}, rest}
           end
       end
     else
-      {{:error, "Error: return keyword missed"}, rest}
+      {{:error, "Error: return keyword missed in line " <> Integer.to_string(lineRet)}, rest}
     end
   end
 
-  def parse_expression([next_token | rest]) do
+
+
+  def parse_expression([tup_token | rest]) do
+    out=while_expression([tup_token | rest], nil)
+
+    tree=elem(out, 0)
+    rest=elem(out, 1)
+
+        {tree, rest}
+
+  end
+
+  def while_expression([tup_token | rest], tree) do
+
+    term=while_term([tup_token | rest], tree)
+
+    left=elem(term, 0)
+    rest=elem(term, 1)
+
+    [tup_token | rest] = rest
+    next_token=elem(tup_token, 0)
+
+    if next_token == :addition do
+
+      term=while_term(rest, tree)
+
+      right=elem(term, 0)
+      rest=elem(term, 1)
+
+      new_tree=%AST{node_name: :addition, left_node: left, right_node: right}
+
+      [tup_token | rst] = rest
+      next_token=elem(tup_token, 0)
+
+      if next_token == :addition do
+
+        tree_rest=while_expression(rst, new_tree)
+        tree=elem(tree_rest, 0)
+        rest=elem(tree_rest, 1)
+          {%AST{node_name: :addition, left_node: new_tree, right_node: tree}, rest}
+
+      else
+        if next_token == :minus do
+
+          tree_rest=while_expression(rst, new_tree)
+          tree=elem(tree_rest, 0)
+          rest=elem(tree_rest, 1)
+          {%AST{node_name: :minus, left_node: new_tree, right_node: tree}, rest}
+
+        else
+
+          {new_tree, rest}
+        end
+      end
+    else
+      if next_token == :minus do
+
+        term=while_term(rest, tree)
+        right=elem(term, 0)
+        rest=elem(term, 1)
+        new_tree=%AST{node_name: :minus, left_node: left, right_node: right}
+        [tup_token | rst] = rest
+        next_token=elem(tup_token, 0)
+
+        if next_token == :addition do
+          tree_rest=while_expression(rst, new_tree)
+          tree=elem(tree_rest, 0)
+          rest=elem(tree_rest, 1)
+          {%AST{node_name: :addition, left_node: new_tree, right_node: tree}, rest}
+
+        else
+          if next_token == :minus do
+            tree_rest=while_expression(rst, new_tree)
+            tree=elem(tree_rest, 0)
+            rest=elem(tree_rest, 1)
+            {%AST{node_name: :minus, left_node: new_tree, right_node: tree}, rest}
+
+
+          else
+            {new_tree, rest}
+          end
+        end
+
+
+
+      else
+        {left, [tup_token | rest]}
+      end
+
+    end
+
+  end
+
+  def while_term([tup_token | rest], _tree) do
+
+    term=parse_factor([tup_token | rest])
+    left=elem(term, 0)
+    rest=elem(term, 1)
+    [tup_token | rest] = rest
+    next_token=elem(tup_token, 0)
+
+    if next_token == :multiplication do
+
+      term=parse_factor(rest)
+      right=elem(term, 0)
+      rest=elem(term, 1)
+      new_tree=%AST{node_name: :multiplication, left_node: left, right_node: right}
+      [tup_token | rst] = rest
+      next_token=elem(tup_token, 0)
+      if next_token == :multiplication do
+        tree_rest=while_term(rst, new_tree)
+        tree=elem(tree_rest, 0)
+        rest=elem(tree_rest, 1)
+        {%AST{node_name: :multiplication, left_node: new_tree, right_node: tree}, rest}
+
+      else
+        if next_token == :division do
+          tree_rest=while_term(rst, new_tree)
+          tree=elem(tree_rest, 0)
+          rest=elem(tree_rest, 1)
+          {%AST{node_name: :division, left_node: new_tree, right_node: tree}, rest}
+
+        else
+          {new_tree, rest}
+        end
+      end
+
+    else
+      if next_token == :division do
+
+        term=parse_factor(rest)
+        right=elem(term, 0)
+        rest=elem(term, 1)
+        new_tree=%AST{node_name: :division, left_node: left, right_node: right}
+        [tup_token | rst] = rest
+        next_token=elem(tup_token, 0)
+        if next_token == :multiplication do
+          tree_rest=while_term(rst, new_tree)
+          tree=elem(tree_rest, 0)
+          rest=elem(tree_rest, 1)
+          {%AST{node_name: :multiplication, left_node: new_tree, right_node: tree}, rest}
+
+        else
+          if next_token == :division do
+            tree_rest=while_term(rst, new_tree)
+            tree=elem(tree_rest, 0)
+            rest=elem(tree_rest, 1)
+            {%AST{node_name: :division, left_node: new_tree, right_node: tree}, rest}
+
+          else
+            {new_tree, rest}
+          end
+        end
+
+
+      else
+
+        {left, [tup_token | rest]}
+      end
+    end
+
+  end
+
+  def parse_factor([tup_token | rest]) do
+
+    next_token=elem(tup_token, 0)
+    line_con=elem(tup_token, 1)
+
     case next_token do
-      {:constant, value} -> {%AST{node_name: :constant, value: value}, rest}
-      _ -> {{:error, "Error: constant value missed"}, rest}
+      :open_paren ->
+        expression = parse_expression(rest)
+        tree=elem(expression, 0)
+        rest=elem(expression, 1)
+        [tup_token | rest] = rest
+        next_token_tmp=elem(tup_token,0)
+        lineCloseParen=elem(tup_token, 1)
+        if next_token_tmp != :close_paren do
+          {{:error, "Error: close parenthesis missing at line " <> Integer.to_string(lineCloseParen)}, rest}
+        else
+        {tree, rest}
+        end
+      :minus ->
+
+        [tup_token | rest] = rest
+        factor=parse_factor([tup_token | rest])
+        constant_node=elem(factor,0)
+        rest=elem(factor, 1)
+
+        {%AST{node_name: :minus, left_node: constant_node}, rest}
+      :bitwise ->
+        [tup_token | rest] = rest
+        factor=parse_factor([tup_token | rest])
+        constant_node=elem(factor, 0)
+        rest=elem(factor, 1)
+        {%AST{node_name: :bitwise, left_node: constant_node}, rest}
+      :logical_negation ->
+        [tup_token | rest] = rest
+        #next_token_tmp=elem(tup_token,0)
+        factor=parse_factor([tup_token | rest])
+        constant_node=elem(factor,0)
+        rest=elem(factor, 1)
+        {%AST{node_name: :logical_negation, left_node: constant_node}, rest}
+      {:constant, value} ->
+
+        {%AST{node_name: :constant, value: value}, rest}
+      _ ->
+        {{:error, "Error: missing constant at line " <> Integer.to_string(line_con)}, rest}
+
     end
   end
 end
-
-#holaaaaaaaaa
